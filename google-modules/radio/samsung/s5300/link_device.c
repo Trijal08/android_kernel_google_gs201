Aarqw12/android_kernel_google_zuma

s5300: Fix compilation when built as an integrated module
create_link_device() is also the name of a global function in the GNSS
driver. Rename the s5300 version so that it doesn't conflict with the GNSS
definition.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

google-modules/radio/samsung/s5300/link_device.c
@@ -3988,7 +3988,7 @@ static int parse_ect_tables(struct platform_device *pdev,
 	return 0;
 }
 
-struct link_device *create_link_device(struct platform_device *pdev, u32 link_type)
+struct link_device *s5300_create_link_device(struct platform_device *pdev, u32 link_type)
 {
 	struct modem_data *modem;
 	struct mem_link_device *mld;

google-modules/radio/samsung/s5300/modem_variation.h
@@ -34,12 +34,12 @@
 
 
 #define DECLARE_LINK_INIT()						\
-	struct link_device *create_link_device(				\
+	struct link_device *s5300_create_link_device(			\
 				struct platform_device *pdev,		\
 				u32 link_type)
 
 #define DECLARE_LINK_INIT_DUMMY()					\
-	struct link_device *dummy_create_link_device(			\
+	struct link_device *s5300_dummy_create_link_device(		\
 				struct platform_device *pdev,		\
 				u32 link_type)				\
 	{ return NULL; }
@@ -48,8 +48,8 @@
 
 #define MODEM_UNINIT_CALL(type) type ## _uninit_modemctl_device
 
-#define LINK_INIT_CALL()	create_link_device
-#define LINK_INIT_CALL_DUMMY()	dummy_create_link_device
+#define LINK_INIT_CALL()	s5300_create_link_device
+#define LINK_INIT_CALL_DUMMY()	s5300_dummy_create_link_device
 
 
 /**

Trijal08/android_kernel_google_gs201

google-modules/radio/samsung/s5300/link_device.c
@@ -4098,7 +4098,7 @@ static int parse_ect_tables(struct platform_device *pdev,
 	return 0;
 }
 
-struct link_device *create_link_device(struct platform_device *pdev, u32 link_type)
+struct link_device *s5400_create_link_device(struct platform_device *pdev, u32 link_type)
 {
 	struct modem_data *modem;
 	struct mem_link_device *mld;

google-modules/radio/samsung/s5300/modem_variation.h
@@ -34,12 +34,12 @@
 
 
 #define DECLARE_LINK_INIT()						\
-	struct link_device *create_link_device(				\
+	struct link_device *s5400_create_link_device(			\
 				struct platform_device *pdev,		\
 				u32 link_type)
 
 #define DECLARE_LINK_INIT_DUMMY()					\
-	struct link_device *dummy_create_link_device(			\
+	struct link_device *s5400_dummy_create_link_device(		\
 				struct platform_device *pdev,		\
 				u32 link_type)				\
 	{ return NULL; }
@@ -48,8 +48,8 @@
 
 #define MODEM_UNINIT_CALL(type) type ## _uninit_modemctl_device
 
-#define LINK_INIT_CALL()	create_link_device
-#define LINK_INIT_CALL_DUMMY()	dummy_create_link_device
+#define LINK_INIT_CALL()	s5400_create_link_device
+#define LINK_INIT_CALL_DUMMY()	s5400_dummy_create_link_device
 
 
 /**